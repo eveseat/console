d4d11600436e4baf44b6169fa575706963d6dec9
mrurb <minikaliffen@gmail.com>
2017-01-25 20:36:18 +0100

    Fixed UpdateSde not using port (#3)
    * Fixed UpdateSde not using port
    
    Fixed UpdateSde not using port
    
    * Added the port to the console output
    
    * Using spaces rather then tabs to ident lines

--- src/Commands/Eve/UpdateSde.php
+++ src/Commands/Eve/UpdateSde.php
@@ -178,7 +178,8 @@ class UpdateSde extends Command
         $this->line('');
         $this->info('The SDE will be imported to mysql://' .
             config('database.connections.mysql.username') . '@' .
-            config('database.connections.mysql.host') . '/' .
+            config('database.connections.mysql.host') . ':' .
+            config('database.connections.mysql.port') . '/' .
             config('database.connections.mysql.database'));
 
         if (! $this->confirm('Does the above look OK?', true)) {
@@ -375,6 +376,7 @@ class UpdateSde extends Command
                 // Append this regardless. Escape special chars in the password too.
                 . escapeshellcmd(config('database.connections.mysql.password')) .
                 ' -h ' . config('database.connections.mysql.host') .
+                ' -P ' . config('database.connections.mysql.port') .
                 ' ' . config('database.connections.mysql.database') .
                 ' < ' . $extracted_path;
 
