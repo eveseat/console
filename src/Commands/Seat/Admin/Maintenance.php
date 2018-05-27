2fd4efab55f284d1843867e25f747a286edf5a7b
Leon Jacobs <leonja511@gmail.com>
2018-05-27 17:47:59 +0200

    Add maintenence command file.

---
+++ src/Commands/Seat/Admin/Maintenance.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Console\Commands\Seat\Admin;
+
+use Illuminate\Console\Command;
+use Seat\Services\Jobs\Maintenance as MaintenanceJob;
+
+/**
+ * Class Maintenance.
+ * @package Seat\Console\Commands\Seat\Admin
+ */
+class Maintenance extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'seat:admin:maintenance';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Dispatches a maintenance job';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+        dispatch((new MaintenanceJob))->onQueue('medium');
+    }
+}
