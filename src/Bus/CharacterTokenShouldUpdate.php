935d2904c15bf7bd64baeb283b5e893e175e1264
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-07-19 21:00:43 +0200

    refactor: remove stats job which have been drop by CCP

--- src/Bus/CharacterTokenShouldUpdate.php
+++ src/Bus/CharacterTokenShouldUpdate.php
@@ -39,7 +39,6 @@ use Seat\Eveapi\Jobs\Character\Medals;
 use Seat\Eveapi\Jobs\Character\Notifications;
 use Seat\Eveapi\Jobs\Character\Roles;
 use Seat\Eveapi\Jobs\Character\Standings;
-use Seat\Eveapi\Jobs\Character\Stats;
 use Seat\Eveapi\Jobs\Character\Titles;
 use Seat\Eveapi\Jobs\Clones\Clones;
 use Seat\Eveapi\Jobs\Clones\Implants;
@@ -134,7 +133,6 @@ class CharacterTokenShouldUpdate extends BusCommand
         Notifications::dispatch($this->token)->onQueue($this->queue);
         Roles::dispatch($this->token)->onQueue($this->queue);
         Standings::dispatch($this->token)->onQueue($this->queue);
-        Stats::dispatch($this->token)->onQueue($this->queue);
         Titles::dispatch($this->token)->onQueue($this->queue);
 
         // Clones
